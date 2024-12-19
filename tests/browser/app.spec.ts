345f107525e2e1a3143e946fa2ca66e297ee5ed9
Huy-Thai <huythai.dev@gmail.com>
2024-12-19 11:26:47 +0700

    config vitest with browser and unit testing

---
+++ tests/browser/app.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest'
+// import { render } from 'vitest-browser-vue'
+// import App from '../../src/App.vue'
+
+test('testing for UI', async () => {
+    // const { getByText, getByRole } = render(App)
+    //
+    // await expect.element(getByText('Hello Vitest x1!')).toBeInTheDocument()
+    //
+    // await getByRole('button', { name: 'Increment' }).click()
+    //
+    // await expect.element(getByText('Hello Vitest x2!')).toBeInTheDocument()
+    expect.assertions(0)
+})
